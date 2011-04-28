8eb18e38646d24d0e05303dc317fc8def96f2660
mr.nuke.me <mr.nuke.me@8eae5a90-4648-11de-84a6-839b6ed6c72e>
2011-04-28 13:36:26 +0000

    Add high precision timer class. Use more flexible profiling information by inserting it in a std::vector

--- include/X-Compat/HPC Timing.h
+++ include/X-Compat/HPC Timing.h
@@ -6,12 +6,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * ElectroMag is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  *  along with ElectroMag.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -49,11 +49,77 @@ inline void QueryHPCTimer(long long *time)
     *time = linTimer.tv_sec * TIMER_FREQUENCY + linTimer.tv_usec;
 }
 
+inline long long QueryHPCTimer()
+{
+    timeval linTimer;
+    gettimeofday(&linTimer, 0);
+    return linTimer.tv_sec * TIMER_FREQUENCY + linTimer.tv_usec;
+}
+
 inline void QueryHPCFrequency(long long *freq)
 {
     *freq = TIMER_FREQUENCY;
 }
+
+inline long long QueryHPCFrequency()
+{
+    return TIMER_FREQUENCY;
+}
 #undef TIMER_FREQUENCY
+
+class PerfTimer {
+private:
+    long long m_startTime;
+    long long m_lastTick;
+    long long m_frequency;
+    double m_elapsed;
+
+    double getTime(long long start, long long end)
+    {
+        return ((double)(end-start))/((double)m_frequency);
+    }
+
+public:
+    PerfTimer() {
+        reset();
+        m_frequency = QueryHPCFrequency();
+    };
+
+    void start() {
+        if (m_startTime == 0) {
+            // Timer was not started, or has been stopped
+            m_elapsed = 0;
+        }
+        m_startTime = m_lastTick = QueryHPCTimer();
+    }
+
+    double pause() {
+        long pause = QueryHPCTimer();
+        m_elapsed += getTime(m_startTime, pause);
+        return m_elapsed;
+    }
+
+    double stop() {
+        pause();
+        reset();
+        return m_elapsed;
+    }
+    
+    void reset() {
+        m_startTime = m_lastTick = 0;
+    }
+
+    double getElapsed() {
+        return m_elapsed;
+    }
+    
+    double tick() {
+        long long tick = QueryHPCTimer();
+        double elapsed = getTime(m_lastTick, tick);
+        m_lastTick = tick;
+        return elapsed;
+    }
+};
 
 #else
 #error Compilation platform not found or not supported. \
